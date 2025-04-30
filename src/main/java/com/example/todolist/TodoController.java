e3e6483ca8aba6b0c0f1cf549e547b994b111da3
JaideepReddyAddula <jaideepaddula28@gmail.com>
2025-04-30 17:04:30 +0530

    Update TodoController.java

--- src/main/java/com/example/todolist/TodoController.java
+++ src/main/java/com/example/todolist/TodoController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://127.0.0.1:5501")
+@CrossOrigin(origins = "https://jaideepreddyaddula.github.io")
 @RestController
 @RequestMapping("/api/todos")
 public class TodoController {
