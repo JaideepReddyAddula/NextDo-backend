92b4cf74d3cd584684ec214ab353350774a00ee3
JaideepReddyAddula <jaideepaddula28@gmail.com>
2025-04-30 19:09:50 +0530

    Update TodoController.java

--- src/main/java/com/example/todolist/TodoController.java
+++ src/main/java/com/example/todolist/TodoController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "https://jaideepreddyaddula.github.io")
+@CrossOrigin(origins = "https://getnextdo.netlify.app/")
 @RestController
 @RequestMapping("/api/todos")
 public class TodoController {
